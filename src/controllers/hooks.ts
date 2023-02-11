903750917384743544b36411a752b9c3f7cdc025
ikmalsuzali <ikmalsuzali@gmail.com>
2023-02-12 00:14:00 +0800

    updating the routes

---
+++ src/controllers/hooks.ts
@@ -0,0 +1,52 @@
+// @ts-nocheck
+import { FastifyReply } from "fastify";
+import { FastifyInstance, FastifyRequest } from "fastify";
+import { prisma } from "../prisma";
+import "../helpers/bigInt.js";
+
+export function hookRoutes(fastify: FastifyInstance) {
+  fastify.post(
+    "/api/v1/hook/create-pet",
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      let event = request.body;
+      console.log(request);
+      console.log(event);
+    }
+  );
+
+  const handleEmailPetCreation = (payload) => {
+
+  }
+ 
+  //   {
+  //     type: 'INSERT',
+  //     table: 'pets',
+  //     record: {
+  //       id: '00379472-368c-4f60-8acf-467522c1945b',
+  //       name: 'test',
+  //       email: 'test@gmail.com',
+  //       gender: 0,
+  //       height: 10,
+  //       status: 0,
+  //       weight: 30,
+  //       address: null,
+  //       twitter: '',
+  //       user_id: '54e0ecf1-6f25-415a-ae40-3efa8c5a3bab',
+  //       breed_id: null,
+  //       facebook: '',
+  //       latitude: null,
+  //       instagram: '',
+  //       longitude: null,
+  //       lost_date: null,
+  //       created_at: '2023-02-11T15:10:23.160316+00:00',
+  //       found_date: null,
+  //       is_deleted: null,
+  //       description: 'test',
+  //       is_vaccinated: true,
+  //       animal_type_id: 89,
+  //       contact_number: '317-797-9869'
+  //     },
+  //     schema: 'public',
+  //     old_record: null
+  //   }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -2,9 +2,11 @@ import { FastifyInstance, FastifyRequest, FastifyReply } from "fastify";
 import { petRoutes } from "./controllers/pets";
 import { stripeRoutes } from "./controllers/stripe";
 import { fbAdRoutes } from "./controllers/fbAds";
+import { hookRoutes } from "./controllers/hooks";
 
 export function registerRoutes(fastify: FastifyInstance) {
   petRoutes(fastify);
   stripeRoutes(fastify);
   fbAdRoutes(fastify);
+  hookRoutes(fastify);
 }
